6a488b8ee277d163881b1343c35a73e8e2f9de98
LRP2004 <lsruizpena@gmail.com>
2025-09-28 15:15:48 -0400

    Add OperativoElementos table

---
+++ TerrazasManager/Db/Models/OperativoElementos.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrazasManager.Db.Models
+{
+    [Table("OperativoElementos")]
+    public class OperativoElementos
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        [Required]
+        public int OperativoId { get; set; }
+
+        [Required]
+        public int CasaId { get; set; }
+        
+        public string Elementos { get; set; }
+
+    }
+}
+
